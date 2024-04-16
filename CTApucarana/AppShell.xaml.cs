b7f2bd264fd1082eeefc1cae8fba7d52d689914e
Matheus <matheusvenanciodc@gmail.com>
2024-04-16 10:54:05 -0300

    classe Results feita

---
+++ CTApucarana/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CTApucarana;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CTApucarana/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CTApucarana;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
